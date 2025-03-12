0be20fa2c9f04169dab14e560bef90ee67ac4b96
sniekdhoshafiq <developershafiq9578@gmail.com>
2025-03-12 08:20:53 +0600

    complete video's section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// categories
 function loadCategories() {
   // 1. fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -8,6 +9,14 @@ function loadCategories() {
     .then((data) => displayCategories(data.categories));
 }
 
+// videos
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
+}
+
+// displayCategories ()
 function displayCategories(categories) {
   // get the container
   const categoryContainer = document.getElementById("category-container");
@@ -26,4 +35,44 @@ function displayCategories(categories) {
   }
 }
 
+// displayVideos()
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videos.forEach((video) => {
+    const videoDiv = document.createElement("div");
+    videoDiv.innerHTML = `
+    <div class="card bg-base-100">
+
+            <figure class="relative">
+                <img class="w-full h-[150px] object-cover" src="${video.thumbnail}" alt="Image load soon" />
+                <span class="absolute bottom-2 right-2 text-sm text-white bg-black rounded px-2">3hrs 56 min ago</span>
+            </figure>
+
+            <div class="flex gap-3 px-0 py-3">
+
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                            <img src="${video["authors"][0]["profile_picture"]}" />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="intro">
+                    <h1 class="text-sm font-semibold">${video.title}</h1>
+                    <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}<img class="w-5 h-5"
+                            src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
+                    <p class="text-sm text-gray-400">${video.others.views}</p>
+                </div>
+            </div>
+        </div>
+    `;
+
+    videoContainer.appendChild(videoDiv);
+  });
+};
+
 loadCategories();
+
+loadVideos();
